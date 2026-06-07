b8dd4f234e879b9bf71a253294c5393c2ba3bf02
Azzurdya <suriraaditya08@gmail.com>
2026-06-07 18:37:50 +1200

    fixed main and added templates

--- src/main.py
+++ src/main.py
@@ -1,26 +1,45 @@
 import os
+import re
 import shutil
 
 import markdown_to_html
 
 
 def main():
-    public_files = os.listdir("../public")
-    source_files = os.listdir("../static")
+    source = os.getcwd()
+    print(source)
+    public_files = os.listdir(f"{source}/public")
+    source_files = os.listdir(f"{source}/static")
     print(public_files)
     for file in public_files:
-        if os.path.exists(f"../public/{file}"):
-            os.remove(f"../public/{file}")
+        if os.path.exists(f"{source}/public/{file}"):
+            os.remove(f"{source}/public/{file}")
     for file in source_files:
-        if os.path.exists(f"../static/{file}"):
-            shutil.copy(f"../static/{file}", f"../public/{file}")
+        if os.path.exists(f"{source}/static/{file}"):
+            shutil.copy(f"{source}/static/{file}", f"{source}/public/{file}")
         if file == "source.html":
-            markdown = open(f"../src/source.md", "r").read()
-            html = f"<html><body>{markdown_to_html.markdown_to_html_node(markdown)}</body></html>"
+            markdown = open(f"{source}/src/source.md", "r").read()
+            template = open(f"{source}/template.html", "r").read()
+            html = html_wrapper(markdown, template)
             print(html)
-            open(f"../public/{file}", "w").write(html)
-    new_public_files = os.listdir("../public")
+            open(f"{source}/public/{file}", "w").write(html)
+    new_public_files = os.listdir(f"{source}/public")
     return new_public_files
+
+
+def heading_extracter(html):
+    head_text = re.findall(r"<h1>(.*?)</h1>", html)[0]
+    html = re.sub(r"<h1># (.*?)</h1>", r"", html)
+    head_text = re.sub(r"#", r"", head_text).strip()
+    return head_text, html
+
+
+def html_wrapper(markdown, template):
+    html = markdown_to_html.markdown_to_html_node(markdown)
+    head_text, html = heading_extracter(html)
+    template = re.sub(r"{{ Title }}", head_text, template)
+    template = re.sub(r"{{ Content }}", html, template)
+    return template
 
 
 print(main())
--- src/source.md
+++ src/source.md
@@ -1,21 +1,46 @@
-This is **bolded** paragraph
-text in a p
-tag here
+# Tolkien Fan Club
 
-This is another paragraph with _italic_ text and `code` here
+![JRR Tolkien sitting](/images/tolkien.png)
+
+Here's the deal, **I like Tolkien**.
+
+> "I am in fact a Hobbit in all but size."
+>
+> -- J.R.R. Tolkien
+
+## Blog posts
+
+- [Why Glorfindel is More Impressive than Legolas](/blog/glorfindel)
+- [Why Tom Bombadil Was a Mistake](/blog/tom)
+- [The Unparalleled Majesty of "The Lord of the Rings"](/blog/majesty)
+
+## Reasons I like Tolkien
+
+- You can spend years studying the legendarium and still not understand its depths
+- It can be enjoyed by children and adults alike
+- Disney _didn't ruin it_ (okay, but Amazon might have)
+- It created an entirely new genre of fantasy
+
+## My favorite characters (in order)
+
+1. Gandalf
+2. Bilbo
+3. Sam
+4. Glorfindel
+5. Galadriel
+6. Elrond
+7. Thorin
+8. Sauron
+9. Aragorn
+
+Here's what `elflang` looks like (the perfect coding language):
 
 ```
-this should be code **and not bold**
+func main(){
+    fmt.Println("Aiya, Ambar!")
+}
 ```
 
-- item 1
-- item 2
-- item 3
+Want to get in touch? [Contact me here](/contact).
 
-1. item 1
-2. item 2
-3. item 3
-
-1. item 1
-3. item 3
-2. item 2
+This site was generated with a custom-built [static site generator](https://www.boot.dev/courses/build-static-site-generator-python) from the course on [Boot.dev](https://www.boot.dev).
